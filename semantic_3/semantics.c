f048c69dbd1ac13395bb0593a285d98a41cceb12
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-06-29 15:27:44 +0700

    Sematic 3: part 2

--- semantic_3/semantics.c
+++ semantic_3/semantics.c
@@ -53,26 +53,127 @@ Object* checkDeclaredIdent(char* name) {
 }
 
 Object* checkDeclaredConstant(char* name) {
-  // TODO
+    Object *obj = NULL;
+
+    // Start search
+    on_search = 0;
+
+    do {
+        obj = lookupObject(name);
+        if (obj != NULL && obj->kind == OBJ_CONSTANT)
+            break;
+    } while (obj != NULL);
+
+    // End search
+    on_search = 0;
+
+    if (obj == NULL)
+        error(ERR_UNDECLARED_CONSTANT, currentToken->lineNo, currentToken->colNo);
+
+    return obj;
 }
 
 Object* checkDeclaredType(char* name) {
-  // TODO
+    Object *obj = NULL;
+
+    // Start search
+    on_search = 0;
+
+    do {
+        obj = lookupObject(name);
+        if (obj != NULL && obj->kind == OBJ_TYPE)
+            break;
+    } while (obj != NULL);
+
+    // End search
+    on_search = 0;
+
+    if (obj == NULL)
+        error(ERR_UNDECLARED_TYPE, currentToken->lineNo, currentToken->colNo);
+
+    return obj;
 }
 
 Object* checkDeclaredVariable(char* name) {
-  // TODO
+    Object *obj = NULL;
+
+    // Start search
+    on_search = 0;
+
+    do {
+        obj = lookupObject(name);
+        if (obj != NULL && obj->kind == OBJ_VARIABLE)
+            break;
+    } while (obj != NULL);
+
+    // End search
+    on_search = 0;
+
+    if (obj == NULL)
+        error(ERR_UNDECLARED_VARIABLE, currentToken->lineNo, currentToken->colNo);
+
+    return obj;
 }
 
 Object* checkDeclaredFunction(char* name) {
-  // TODO
+    Object *obj = NULL;
+
+    // Start search
+    on_search = 0;
+
+    do {
+        obj = lookupObject(name);
+        if (obj != NULL && obj->kind == OBJ_FUNCTION)
+            break;
+    } while (obj != NULL);
+
+    // End search
+    on_search = 0;
+
+    if (obj == NULL)
+            error(ERR_UNDECLARED_FUNCTION, currentToken->lineNo, currentToken->colNo);
+
+    return obj;
 }
 
 Object* checkDeclaredProcedure(char* name) {
-  // TODO
+    Object *obj = NULL;
+
+    // Start search
+    on_search = 0;
+
+    do {
+        obj = lookupObject(name);
+        if (obj != NULL && obj->kind == OBJ_PROCEDURE)
+            break;
+    } while (obj != NULL);
+
+    // End search
+    on_search = 0;
+
+    if (obj == NULL)
+            error(ERR_UNDECLARED_PROCEDURE, currentToken->lineNo, currentToken->colNo);
+
+    return obj;
 }
 
 Object* checkDeclaredLValueIdent(char* name) {
-  // TODO
-}
+    Object *obj = NULL;
 
+    // Start search
+    on_search = 0;
+
+    do {
+        obj = lookupObject(name);
+        if (obj != NULL && (obj->kind == OBJ_FUNCTION || obj->kind == OBJ_PARAMETER || obj->kind == OBJ_VARIABLE))
+            break;
+    } while (obj != NULL);
+
+    // End search
+    on_search = 0;
+
+    if (obj == NULL)
+            error(ERR_UNDECLARED_IDENT, currentToken->lineNo, currentToken->colNo);
+
+    return obj;
+}
